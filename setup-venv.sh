df5bd0ba2622f461f3b002041f1933d2c8e95f1f
andlarsen <dreaslarsen93@gmail.com>
2025-06-05 14:09:13 +0200

    added setup-venv.sh

--- setup-venv.sh
+++ setup-venv.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+# source the virtual environment if it exists
+if [ -d "venv" ]; then
+    echo "Activating existing virtual environment..."
+    source venv/bin/activate
+    echo "Virtual environment activated."
+else
+    echo "No existing virtual environment found."
+    echo "Creating a new virtual environment..."
+    python3 -m venv venv
+    echo "Activating existing virtual environment..."
+    source venv/bin/activate
+    echo "Virtual environment activated."
+fi
+
+echo "Installing required packages..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Virtual environment setup complete."
