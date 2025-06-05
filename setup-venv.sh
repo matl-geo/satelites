cffa64cd151c3c32fa47aa982149b3ec68860842
andlarsen <dreaslarsen93@gmail.com>
2025-06-05 16:25:36 +0200

    separated pip and venv

--- setup-venv.sh
+++ setup-venv.sh
@@ -5,10 +5,28 @@ if ! command -v python3 &> /dev/null; then
     echo "Python 3 is not installed. Installing Python 3..."
     sudo apt update
     sudo apt upgrade -y
-    sudo apt install -y python3 python3-venv python3-pip
+    sudo apt install -y python3
     echo "Python 3 installed successfully."
 else
     echo "Python 3 is already installed."
+fi  
+
+# Install python3-venv if not already installed
+if ! python3 -m venv --help &> /dev/null; then
+    echo "python3-venv is not installed. Installing python3-venv..."
+    sudo apt install -y python3-venv
+    echo "python3-venv installed successfully."
+else
+    echo "python3-venv is already installed."
+fi
+
+# Install python3-pip if not already installed
+if ! command -v pip3 &> /dev/null; then
+    echo "pip3 is not installed. Installing pip3..."
+    sudo apt install -y python3-pip
+    echo "pip3 installed successfully."
+else
+    echo "pip3 is already installed."
 fi  
 
 # source the virtual environment if it exists
