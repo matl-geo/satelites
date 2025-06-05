81a820925ead855c45974abdb119d051f65b4d36
andlarsen <dreaslarsen93@gmail.com>
2025-06-05 16:24:18 +0200

    removed too much

--- setup-venv.sh
+++ setup-venv.sh
@@ -11,6 +11,19 @@ else
     echo "Python 3 is already installed."
 fi  
 
+# source the virtual environment if it exists
+if [ -d "venv" ]; then
+    echo "Activating existing virtual environment..."
+    source venv/bin/activate
+    echo "Virtual environment activated."
+else
+    echo "No existing virtual environment found."
+    echo "Creating a new virtual environment..."
+    python3 -m venv venv
+    echo "Activating existing virtual environment..."
+    source venv/bin/activate
+    echo "Virtual environment activated."
+fi
 
 echo "Installing required packages..."
 pip install --upgrade pip
